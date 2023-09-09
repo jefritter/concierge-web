ff9cbaf93cd92378a852228d1578ba6ac0bc2944
Heather Young <hjy.earth@gmail.com>
2023-09-09 10:03:37 -0600

    initial set-up with api call

---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>{{ header }}</h1>
+    <table v-if="hasUserData">
+      <tr>
+        <th>Id</th>
+        <th>First</th>
+        <th>Last</th>
+        <th>Email</th>
+      </tr>
+      <tr v-for="(user, index) in users" :key="index">
+        <td>{{ user.id }}</td>
+        <td>{{ user.first_name }}</td>
+        <td>{{ user.last_name }}</td>
+        <td>{{ user.email }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        header: 'Users',
+        users: []
+      }
+    },
+    beforeMount() {
+      fetch('/api/users/all')
+        .then(response => response.json())
+        .then(data => this.users = data.data)
+    },
+    computed: {
+      hasUserData() {
+        return this.users.length > 0
+      }
+    }
+  }
+</script>
+
+<style lang="">
+  
+</style>
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  build: {
+    sourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), 
+        ws: false
+      }
+    }
+  }
+})
