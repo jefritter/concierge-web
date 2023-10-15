798ae1b547bff51a6f8b84eaff57fbfd80590956
Panharith257 <p_menh@mail.fhsu.edu>
2023-10-15 23:42:58 +0700

    Update Dining.vue

--- src/components/services/Dining.vue
+++ src/components/services/Dining.vue
@@ -1,71 +1,37 @@
-<!-- eslint-disable vue/no-unused-components -->
-<!-- eslint-disable vue/no-unused-vars -->
-<!-- eslint-disable vue/require-v-for-key -->
 <template>
-  <div class="dining-home">
-    <div>
-      <CustomHeader title="My Concierge" subtitle="Dining" />
-    </div>
-
-
-    <div class="navbar-container">
-      <nav>
-        <ul>
-          <li><a><router-link to="/">Home</router-link></a></li>
+  <div class="spa-home">
+    <CustomHeader title="My Concierge" subtitle="Spa" />
+    <nav>
+      <ul>
+        <li><a><router-link to="/">Home</router-link></a></li>
           <li class="sep">|</li>
           <li><a><router-link to="/calendar">Calendar</router-link></a></li>
           <li class="sep">|</li>
-          <li><a><router-link to="/services">Services</router-link></a></li>
-        </ul>
-      </nav>
-    </div>
-
+          <li><a><router-link to="/">Services</router-link></a></li>
+      </ul>
+    </nav>
     <div class="main-container">
       <div class="col1">
-        <div class="form-container">
-          <div class="form-box">
-            <div class="title">
-              <h3>Make a Reservation</h3>
-            </div>
-
-            <div class="row">
-              <input type="text">
-              <input type="text">
-            </div>
-
-            <div class="row">
-              <input type="text">
-              <input type="text">
-            </div>
-
-            <div class="row">
-              <button>Submit</button>
-            </div>
-
-          </div>
-        </div>
-
+        <ReservationForm 
+          :fields="fields"
+          @submitReservation="submitReservation"
+          />
         <div class="contact-container">
           <div class="contact">
             <h3>Contact the Spa: 555-222-5656</h3>
           </div>
         </div>
-
       </div>
-
       <div class="service-container">
         <div class="services">
           <div class="title">
-            <h3>Today's Dinner Specials</h3>
+            <h3>
+              Today's Dinner Specials
+            </h3>
           </div>
-
-          <div v-for="(item, index) in items" :key="index">
-            <ul>
-              <li>
-              <h4 class="event-title">{{ item.title }}</h4>
-              <h4>{{ item.food }}</h4>
-            </li>
-            </ul>
+          <div v-for="(item, index) in items" :key="index" class="event-item">
+            <h4 class="event-title">{{ item.title }}</h4>
+            <p><strong></strong> <span>{{ item.food }}</span></p>
           </div>
         </div>
       </div>
@@ -75,109 +41,114 @@
 
 <script>
 import CustomHeader from '../shared/CustomHeader.vue';
+import ReservationForm from '../shared/ReservationForm.vue'
 
 export default {
   components: {
-    CustomHeader
+    CustomHeader,
+    ReservationForm
   },
   data() {
     return {
       items: [
-        {
+        { 
+          id: 1,
           title: 'Soup of the Day',
           food: 'Boullabaise'
         },
-        {
+        { 
+          id: 2,
           title: 'Seafood',
           food: 'Filet of Sole'
         },
-        {
+        { 
+          id: 3,
           title: 'Steak',
           food: 'Filet Mignon'
+        }
+      ],
+      fields: [
+        {
+          id: 'date',
+          label: 'Date',
+          type: 'date'
+        },
+        {
+          id: 'time',
+          label: 'Time',
+          type: 'time'
+        },
+        {
+          id: 'name',
+          label: 'Name',
+          type: 'text'
+        },
+        {
+          id: 'service',
+          label: 'Service',
+          type: 'multi-select',
+          options: []
         }
       ]
     };
   },
+  beforeMount() {
+    this.fields[3].options = this.items
+  },
+  methods: {
+      submitReservation(values) {
+        const isValid = this.validateInputs(values)
+        if (isValid) {
+          console.log('submitting')
+        }
+      },
+      validateInputs(values) {
+        return !!values;
+      }
+    }
 }
 </script>
   
 <style lang="scss">
-.dining-home {
-    background-size: cover;
-    background-image: url("/src/assets/fine-dining.jpg");
-    min-height: 100dvh;
-
-  .navbar-container {
-    li {
+.spa-home {
+  background-size: cover;
+  background-image: url("/src/assets/massage-therapy.jpg");
+  min-height: 100vh;  
+  nav {
+    background-color: white;
+    margin: 0;
+    text-align: center;
+      li {
       display: inline;
       list-style: none;
     }
-
-    nav {
-      background-color: white;
-      margin: 0;
-      text-align: center;
-
-      .sep {
-        color: black;
-      }
-
-      a {
-        text-decoration: none;
-        color: black;
-        /* Link color */
-      }
+    .sep {
+      color: black;
+    }
+    a {
+      text-decoration: none;
+      color: black;
     }
   }
-
   .main-container {
     display: flex;
+    gap: 6rem;
     justify-content: space-around;
     flex-wrap: wrap;
-    padding: 2em;
-
-    .form-container {
-      background-color: white;
-      width: 500px;
-      padding: 0.5em;
-      border: solid 1px black;
-
-      .row {
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-        flex-wrap: wrap;
-        margin: 4em;
-      }
-
-      .row input {
-        width: 150px;
-        height: 40px;
-        border-radius: 8px;
-        border: 1px solid #cccccc;
-        border-color: black;
-      }
-
-      h3 {
-        text-align: center;
-        border-bottom: solid 1px;
-        color: black;
-        padding: 10px;
-      }
+    padding: 8em;
+    .col1 {
+      flex-basis: 50%;
     }
-
     .service-container {
       background-color: white;
-      width: 400px;
       border-radius: 10px;
-
+      flex-basis: 40%;
       .services h3 {
         font-size: x-large;
         text-align: center;
         color: black;
         padding: 10px;
       }
-
       .services h3:after {
         content: ' ';
         display: block;
@@ -186,7 +157,6 @@ export default {
         margin-inline: auto;
         margin-block: 6px;
       }
-
       .services li {
         color: black;
         list-style-type: none;
@@ -195,34 +165,17 @@ export default {
         font-size: large;
       }
     }
-
     .contact-container {
       background-color: white;
-      width: 500px;
+      max-width: 560px;
       padding: 1em;
       border: solid 1px black;
       margin-top: 1em;
-
       .contact h3 {
         text-align: center;
         color: black;
       }
     }
-  }
-
-  .event-item {
-    margin-bottom: 20px;
-    padding: 10px;
-    margin-left: 100px;
-  }
-
-  .event-title {
-    font-weight: bold;
-    font-size: 1.2rem;
-  }
-
-  span {
-    font-size: 20px;
   }
 
   h1 {
@@ -237,18 +190,29 @@ export default {
     text-align: center;
   }
 
-  h4 {
-    font-size: 1.2rem;
-    text-align: center;
-  }
+  .event-item {
+            margin-top: 1rem;
+            padding: 10px;
+            text-align: center;
+          }
+          .event-title {
+            font-weight: bold;
+            font-size: 1.5rem;
+          }
 
   @media (max-width: 767px) {
-
     .form-container,
     .service-container,
     .contact-container {
       width: 300px;
       margin: 1em;
+    }
+  }
+
+  @media (max-width: 1400px) {
+    .main-container {
+      padding: 4rem;
+      gap: 3rem;
     }
   }
 }
